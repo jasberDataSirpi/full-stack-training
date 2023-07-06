8843c707417d16f0569b56dd30b99cef318fa61d
JasberrajaKingPin <jraja@kingpin.global>
2023-07-06 23:03:14 +0530

    login page redesign

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,6 +6,7 @@ import 'package:meal_planet/constants/endpoints.dart';
 import 'package:meal_planet/constants/http_methods.dart';
 import 'package:meal_planet/services/http_service.dart';
 import 'package:meal_planet/services/local_storage_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -44,46 +45,74 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
       body: Container(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                  hintText: 'Enter User Name',
+            const SizedBox(height: 30),
+            Text(
+              'Login',
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: -1,
                 ),
               ),
             ),
-            const SizedBox(height: 16.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
+            const SizedBox(height: 120),
+            SizedBox(
+              width: 370,
+              // height: 56,
+              child: TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  filled: true,
+                  fillColor: Color(0xFFF1F5F5),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 370,
+              // height: 56,
               child: TextField(
                 controller: passwordController,
                 decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
+                  border: InputBorder.none,
+                  filled: true,
+                  fillColor: Color(0xFFF1F5F5),
                   labelText: 'Password',
-                  hintText: 'Enter Password',
                 ),
                 obscureText: true,
               ),
             ),
-            const SizedBox(height: 24.0),
+            const SizedBox(height: 40),
             Container(
-                height: 50,
-                width: 250,
+                width: 370,
+                height: 56,
                 decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(4)),
+                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
                 child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color(0xFF01CBBB)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)))),
                   onPressed: _login,
-                  child: const Text('Login'),
+                  child: const Text(
+                    'Log In',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
                 )),
             const SizedBox(height: 30),
             GestureDetector(
